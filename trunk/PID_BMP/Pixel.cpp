01a8a4c65e588b3c96cd121b0d1cc69bed1f7d76
fernandofernandesantos@gmail.com <fernandofernandesantos@gmail.com@e04f7356-4346-37c6-2087-e2bac6913f2c>
2014-04-17 21:37:38 +0000

    Nova classe pixel

---
+++ trunk/PID_BMP/Pixel.cpp
@@ -0,0 +1,50 @@
+/* 
+ * File:   Pixel.cpp
+ * Author: root
+ * 
+ * Created on 17 de Abril de 2014, 18:10
+ */
+
+#include "Pixel.h"
+
+Pixel::Pixel() {
+}
+
+Pixel::Pixel(const Pixel& orig) {
+}
+
+Pixel::~Pixel() {
+}
+
+unsigned char Pixel::GetB() const {
+    return B;
+}
+
+void Pixel::SetB(unsigned char B) {
+    this->B = B;
+}
+
+unsigned char Pixel::GetG() const {
+    return G;
+}
+
+void Pixel::SetG(unsigned char G) {
+    this->G = G;
+}
+
+unsigned char Pixel::GetR() const {
+    return R;
+}
+
+void Pixel::SetR(unsigned char R) {
+    this->R = R;
+}
+
+unsigned char Pixel::GetT() const {
+    return T;
+}
+
+void Pixel::SetT(unsigned char T) {
+    this->T = T;
+}
+
---
+++ trunk/PID_BMP/Pixel.h
@@ -0,0 +1,34 @@
+/* 
+ * File:   Pixel.h
+ * Author: root
+ *
+ * Created on 17 de Abril de 2014, 18:10
+ */
+
+#ifndef PIXEL_H
+#define	PIXEL_H
+
+class Pixel {
+public:
+    Pixel();
+    Pixel(const Pixel& orig);
+    virtual ~Pixel();
+    unsigned char GetB() const;
+    unsigned char GetG() const;
+    unsigned char GetT() const;
+    unsigned char GetR() const;
+    void SetT(unsigned char T);
+    void SetB(unsigned char T);
+    void SetR(unsigned char T);
+    void SetG(unsigned char T);
+
+private:
+    unsigned char R; // representante da cor vermelha
+    unsigned char G; // representante da cor verde
+    unsigned char B; // representante da cor azul
+    unsigned char T; // representante da transparência em BMPs com 32 bits
+
+};
+
+#endif	/* PIXEL_H */
+
