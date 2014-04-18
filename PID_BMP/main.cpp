9d18075d5d70ade3e47cf5c200868711e3104e92
fernandofernandesantos@gmail.com <fernandofernandesantos@gmail.com@e04f7356-4346-37c6-2087-e2bac6913f2c>
2014-04-18 22:47:11 +0000

    Valor médio

--- PID_BMP/main.cpp
+++ PID_BMP/main.cpp
@@ -18,7 +18,7 @@ using namespace std;
  */
 int main(int argc, char** argv) {
 
-    const char* FILENAM = "CanMan.bmp";
+    const char* FILENAM = "Flores.bmp";
 //    const char* FILENAM = "CanMan.bmp";
     cout << "asd";
    
