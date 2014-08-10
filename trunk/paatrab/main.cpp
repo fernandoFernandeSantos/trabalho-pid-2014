ad6dbd0354d38221f47d015de1c6bcdee4e9741b
fernandofernandesantos@gmail.com <fernandofernandesantos@gmail.com@e04f7356-4346-37c6-2087-e2bac6913f2c>
2014-08-10 19:27:58 +0000

    Primeira fase

---
+++ trunk/paatrab/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: familia
+ *
+ * Created on 10 de Agosto de 2014, 16:27
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
