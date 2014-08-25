2c5dab7f1aa5db30f67dd2e13535b0b3e1447e89
fernandofernandesantos@gmail.com <fernandofernandesantos@gmail.com@e04f7356-4346-37c6-2087-e2bac6913f2c>
2014-08-25 14:21:23 +0000

    Imagem to cinza 90%

--- trunk/PID_BMP/main.cpp
+++ trunk/PID_BMP/main.cpp
@@ -8,10 +8,80 @@
 #include"BMP.h"
 #include <stdlib.h>
 using namespace std;
+ifstream input;
+bool imagemAberta = false;
 
-void histogramOperations(BMP arquivo){
-    
-    
+static void continua() {
+    cout << "Precione [ENTER] para continuar\n";
+    getchar();
+    getchar();
+}
+
+static void histogramOperations(BMP arquivo){
+    if(!input.is_open() || !imagemAberta){
+        cout << "Primeiro abra o arquivo\n";
+        return;
+    }
+    ushort opcao = 1;
+    bool histograma = false;
+    while(opcao){
+        system("clear");
+        cout << "=> Operações com histograma\n" <<
+                " ==> [1] Gerar histograma\n" <<
+                " ==> [2] Equalizar histograma\n" <<
+                " ==> [3] Mostrar histograma (precisa do shotwell instalado)\n"<<
+                " ==> [0] Voltar\n" <<
+                "opção: ";
+        cin >> opcao;
+        char s = 'n';
+        switch (opcao) {
+            case 1:
+            {
+                if(arquivo.makeHistogram()){
+                    cout << "Histograma gerado com sucesso\n";
+                    histograma = true;
+                }else{
+                    cout << "Erro na geração do histograma\n";
+                    continua();
+                    break;
+                }
+                cout << "Gerando visualização...\nA imagem é em escala de cinza[S/n]: ";
+                cin >> s;
+                if(s == 'S'){
+                    arquivo.printHistogram(true);
+                }else{
+                    arquivo.printHistogram(false);
+                }
+                continua();
+                break;
+            }
+            case 2:
+            {
+                if(!histograma){
+                    arquivo.makeHistogram();
+                }
+                if(arquivo.histogramEqualizer()){
+                    cout << "Histograma equalizado\n";
+                }else{
+                    cout << "Problemas na equalização\n";
+                    continua();
+                    break;
+                }
+                cout << "Gerando visualização...\nA imagem é em escala de cinza[S/n]: ";
+                cin >> s;
+                if(s == 'S'){
+                    arquivo.makeHistogram();
+                    arquivo.printHistogram(true);
+                }else{
+                    arquivo.makeHistogram();
+                    arquivo.printHistogram(false);
+                }
+                continua();
+                break;
+            }
+        }
+
+    }
 }
 
 
@@ -36,11 +106,6 @@ static void opcoes() {
             "Opção:";
 }
 
-static void continua() {
-    cout << "Precione [ENTER] para continuar\n";
-    getchar();
-    getchar();
-}
 
 int main(void) {
     BMP arquivo; //arquivo de entrada
@@ -48,174 +113,147 @@ int main(void) {
     char nome[100];
     opcoes();
     scanf("%hu", &opcao);
-    ifstream input;
     while (opcao) {
         switch (opcao) {
-            case 1: //abrir o arquivo
-            {
-                char s = 'S';
-                if (input.is_open()) {
-                    cout << "Já existe um arquivo aberto, deseja abrir outro arquivo S/n : ";
-                    cin >> s;
-                }
-                if (s == 'n')
-                    break;
-                else
-                    input.close();
-                cout << "\nEntre com o nome do arquivo(maxímo 100 caracteres): ";
-                cout << "\n*Caso esteja no diretório do projeto: NomeImagem.bmp\n ";
-                cout << "\n*Caso esteja fora do diretório do projeto: /MeuDiretorio/NomeImagem.bmp\n ";
-                cout << "\nNome:";
-                cin >> nome;
-                try {
-                    input.open(nome, ios::binary);
-                    if (!(input.good() && input.is_open())) {
-                        cout << "Não foi possível abrir o arquivo " << nome << endl;
-                        continua();
-                        break;
-                    }
-                    arquivo.read(&input);
-                } catch (ifstream::failure e) {
-                    cout << "Exception opening/reading/closing file\n";
-                    cout << e.what() << endl;
-                    continua();
-                    break;
-                }
-                break;
+        case 1: //abrir o arquivo
+        {
+            char s = 'S';
+            if (input.is_open()) {
+                cout << "Já existe um arquivo aberto, deseja abrir outro arquivo S/n : ";
+                cin >> s;
             }
-            case 2: //salvar arquivo
-            {
-                if (!input.is_open()) {
-                    cout << "Primeiro abra o arquivo desejado\n";
-                    continua();
-                    break;
-                }
-
-                cout << "\n Entre como nome do arquivo de saida: ";
-                cin >> nome;
-                try {
-                    arquivo.salvar(nome);
-
-                } catch (ifstream::failure e) {
-                    cout << "Exception opening/reading/closing file\n";
-                    cout << e.what() << endl;
-                    continua();
-                    break;
-                }
+            if (s == 'n')
                 break;
-            }
-            case 3: //mostrar cabeçalho do arquivo
-            {
-                if (!input.is_open()) {
-                    cout << "Primeiro abra o arquivo desejado\n";
-                    continua();
-                    break;
-                }
-                arquivo.printCabecalhoArquivo();
-                continua();
-
-                break;
-            }
-            case 4: //mostrar o cabeçalho da imagem
-            {
-                if (!input.is_open()) {
-                    cout << "Primeiro abra o arquivo desejado\n";
-                    continua();
-                    break;
-                }
-                arquivo.printCabecalhoImagem();
-
-                continua();
-                break;
-            }
-            case 5: //valor médio
-            {
+            else
+                input.close();
+            cout << "\nEntre com o nome do arquivo(maxímo 100 caracteres): ";
+            cout << "\n*Caso esteja no diretório do projeto: NomeImagem.bmp\n ";
+            cout << "\n*Caso esteja fora do diretório do projeto: /MeuDiretorio/NomeImagem.bmp\n ";
+            cout << "\nNome:";
+            cin >> nome;
+            try {
+                input.open(nome, ios::binary);
                 if (!(input.good() && input.is_open())) {
-                    cout << "Primeiro abra o arquivo desejado\n";
+                    cout << "Não foi possível abrir o arquivo " << nome << endl;
                     continua();
                     break;
                 }
-                long double *media = arquivo.valorMedio();
-                cout << "Valor médio do R = " << media[0] << endl;
-                cout << "Valor médio do G = " << media[1] << endl;
-                cout << "Valor médio do B = " << media[2] << endl;
+                arquivo.read(&input);
+                imagemAberta = true;
+            } catch (ifstream::failure e) {
+                cout << "Exception opening/reading/closing file\n";
+                cout << e.what() << endl;
                 continua();
                 break;
             }
-            case 6: //variância
-            {
-                if (!input.is_open()) {
-                    cout << "Primeiro abra o arquivo desejado\n";
-                    continua();
-                    break;
-                }
-                long double *variancia = arquivo.variancia(arquivo.valorMedio());
-                cout << "Valor da Variância de R = " << variancia[0] << endl;
-                cout << "Valor da Variância de G = " << variancia[1] << endl;
-                cout << "Valor da Variância de B = " << variancia[2] << endl;
+            break;
+        }
+        case 2: //salvar arquivo
+        {
+            if (!input.is_open()) {
+                cout << "Primeiro abra o arquivo desejado\n";
                 continua();
                 break;
             }
-            case 7: //covariancia
-            {
-                ifstream inputCovariancia;
-                if (!input.is_open()) {
-                    cout << "Primeiro abra o primeiro arquivo\n";
-                    continua();
-                    break;
-                }
-                cout << "\nEntre com o nome do segundo arquivo(maxímo 100 caracteres) ";
-                cout << "\n*Caso esteja no diretório do projeto: NomeImagem.bmp\n ";
-                cout << "\n*Caso esteja fora do diretório do projeto: /MeuDiretorio/NomeImagem.bmp\n ";
-                cout << "\nNome:";
-                cin >> nome;
-                try {
-                    inputCovariancia.open(nome, ios::binary);
-                    if (!(inputCovariancia.good() && inputCovariancia.is_open())) {
-                        cout << "Não foi possível abrir o arquivo " << nome << endl;
-                        continua();
-                        break;
-                    }
-                    BMP imagem2;
-                    imagem2.read(&inputCovariancia);
-                    long double *covariancia = arquivo.covariancia(imagem2);
-                    cout << "A covariancia de R das duas imagems: " << covariancia[0] << endl;
-                    cout << "A covariancia de G das duas imagems: " << covariancia[1] << endl;
-                    cout << "A covariancia de B das duas imagems: " << covariancia[2] << endl;
 
-                    continua();
+            cout << "\n Entre como nome do arquivo de saida: ";
+            cin >> nome;
+            try {
+                arquivo.salvar(nome);
 
-                } catch (ifstream::failure e) {
-                    cout << "Exception opening/reading/closing file\n";
-                    cout << e.what() << endl;
-                    continua();
-                    break;
-                }
+            } catch (ifstream::failure e) {
+                cout << "Exception opening/reading/closing file\n";
+                cout << e.what() << endl;
+                continua();
                 break;
             }
-            case 8:
-            {
+            break;
+        }
+        case 3: //mostrar cabeçalho do arquivo
+        {
+            if (!input.is_open()) {
+                cout << "Primeiro abra o arquivo desejado\n";
+                continua();
                 break;
             }
-            case 9:
-            {
-                
+            arquivo.printCabecalhoArquivo();
+            continua();
+
+            break;
+        }
+        case 4: //mostrar o cabeçalho da imagem
+        {
+            if (!input.is_open()) {
+                cout << "Primeiro abra o arquivo desejado\n";
+                continua();
                 break;
             }
-            case 10:
-            {
+            arquivo.printCabecalhoImagem();
+
+            continua();
+            break;
+        }
+        case 5: //valor médio
+        {
+            if (!(input.good() && input.is_open())) {
+                cout << "Primeiro abra o arquivo desejado\n";
+                continua();
                 break;
             }
-            case 11:
-            {
+            long double *media = arquivo.valorMedio();
+            cout << "Valor médio do R = " << media[0] << endl;
+            cout << "Valor médio do G = " << media[1] << endl;
+            cout << "Valor médio do B = " << media[2] << endl;
+            continua();
+            break;
+        }
+        case 6: //variância
+        {
+            if (!input.is_open()) {
+                cout << "Primeiro abra o arquivo desejado\n";
+                continua();
                 break;
             }
-            case 0:
-            {
-                if(input.is_open())
-                    input.close();
+            long double *variancia = arquivo.variancia(arquivo.valorMedio());
+            cout << "Valor da Variância de R = " << variancia[0] << endl;
+            cout << "Valor da Variância de G = " << variancia[1] << endl;
+            cout << "Valor da Variância de B = " << variancia[2] << endl;
+            continua();
+            break;
+        }
+        case 7: //operações com histograma
+        {
+            if (!input.is_open()) {
+                cout << "Primeiro abra o primeiro arquivo\n";
+                continua();
                 break;
             }
+            histogramOperations(arquivo);
+            break;
+        }
+        case 8:
+        {
+            break;
+        }
+        case 9:
+        {
+
+            break;
+        }
+        case 10:
+        {
+            break;
+        }
+        case 11:
+        {
+            break;
+        }
+        case 0:
+        {
+            if(input.is_open())
+                input.close();
+            break;
+        }
             
 
         }
